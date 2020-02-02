83a981e70eaeb7e4114165a70d55656317eccf32
Pete Russell <pinkbatz@gmail.com>
2020-02-02 18:40:35 +1300

    Initial commit and basic template setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MapComponent } from './map/map.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    PageTitleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-title/page-title.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-page-title',
+  templateUrl: './page-title.component.html',
+  styleUrls: ['./page-title.component.scss']
+})
+export class PageTitleComponent implements OnInit {
+  @Input() pageTitle: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
